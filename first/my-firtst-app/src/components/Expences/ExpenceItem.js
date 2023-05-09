f5356c7782856196a4f94803b7875d2912d0f832
Jakub Rola <jakub.rola@bluesoft.com>
2023-05-09 15:34:39 +0200

    Added clickHandler

--- first/my-firtst-app/src/components/Expences/ExpenceItem.js
+++ first/my-firtst-app/src/components/Expences/ExpenceItem.js
@@ -1,15 +1,24 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenceCal from "./ExpenceCal";
 import Card from "../UI/Card";
 
 function ExpenceItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Chnaged!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenceCal date={props.date}></ExpenceCal>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
